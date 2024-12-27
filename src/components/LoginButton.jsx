c24c24e5717b0689d97bbbc51cf4a8c483661b5b
Ameth Ordoñez <amethgabriel@hotmail.com>
2024-12-27 04:01:02 -0500

    Update variables

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,36 +1,31 @@
 import React from 'react'
 
-const CLIENT_ID = 'd17f7241-8c7a-4235-8d3c-f16486fd300e'
-const REDIRECT_URI = 'http://localhost:8301'
+const CLIENT_ID = import.meta.env.VITE_API_URL
+const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI
 const AUTH_ENDPOINT =
   'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
 
-function generateCodeVerifier() {
-  const array = new Uint8Array(128)
-  window.crypto.getRandomValues(array)
-  return btoa(String.fromCharCode(...array))
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=/g, '')
-}
+// function generateCodeVerifier() {
+//   const array = new Uint8Array(128)
+//   window.crypto.getRandomValues(array)
+//   return btoa(String.fromCharCode(...array))
+//     .replace(/\+/g, '-')
+//     .replace(/\//g, '_')
+//     .replace(/=/g, '')
+// }
 
-async function generateCodeChallenge(codeVerifier) {
-  const encoder = new TextEncoder()
-  const data = encoder.encode(codeVerifier)
-  const hash = await window.crypto.subtle.digest('SHA-256', data)
-  return btoa(String.fromCharCode(...new Uint8Array(hash)))
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=/g, '')
-}
+// async function generateCodeChallenge(codeVerifier) {
+//   const encoder = new TextEncoder()
+//   const data = encoder.encode(codeVerifier)
+//   const hash = await window.crypto.subtle.digest('SHA-256', data)
+//   return btoa(String.fromCharCode(...new Uint8Array(hash)))
+//     .replace(/\+/g, '-')
+//     .replace(/\//g, '_')
+//     .replace(/=/g, '')
+// }
 
 export default function LoginButton() {
   const handleLogin = async () => {
-    const codeVerifier = generateCodeVerifier()
-    const codeChallenge = await generateCodeChallenge(codeVerifier)
-
-    sessionStorage.setItem('code_verifier', codeVerifier)
-
     const authUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(
       REDIRECT_URI
     )}&scope=${encodeURIComponent(
